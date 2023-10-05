252a22c6e0b811a5936174999f5ae984ca11b7a1
TamanaRadmanish <146898177+TamanaRadmanish@users.noreply.github.com>
2023-10-05 12:49:31 -0600

    Update MySecondClass.java

--- src/tek/java/MySecondClass.java
+++ src/tek/java/MySecondClass.java
@@ -4,5 +4,7 @@ public class MySecondClass {
 
     public static void main(String[] args) {
         System.out.println(" my second changing" );
+        System.out.println(" my first pull try");
     }
+
 }
